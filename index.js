011b5b93fbd4b6f46d279829a9ab956caf7d9708
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2017-01-24 20:52:31 +0530

    multer included

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var express = require('express'),
     mongoose = require('mongoose'),
-    Busboy = require('busboy'),
+    multer = require('multer'),
     router = express.Router(),
     cons = require('consolidate'),
     dust = require('dustjs-linkedin'),
@@ -8,6 +8,10 @@ var express = require('express'),
     bodyParser = require('body-parser');
 
 var app = express();
+
+var upload = multer({
+    dest: __dirname + '/img/'
+});
 
 app.engine('dust', cons.dust);
 app.set('view engine', 'dust');
@@ -21,32 +25,15 @@ router.get('/', function(req, res) {
     res.render('index');
 });
 
-router.get('/save', function(req, res) {
-    res.render('save');
-});
 
-
-router.post('/save', function(req, res) {
-    var busboy = new Busboy({ headers: req.headers });
-    var saveTo;
-    var formdata={};
-    busboy.on('file', function(fieldname, file, filename, encoding, mimetype) {
-        saveTo = path.join(__dirname, '/img/', filename);
-        formdata.profileSrc=saveTo;
-         console.log(formdata);
-        file.pipe(fs.createWriteStream(saveTo));
-    });
-    busboy.on('finish', function() {
-         formdata = {
-            "username": req.body.username,
-            "email": req.body.email
-        };
-        console.log(formdata);
-        res.render('save', { formdata: formdata });
-
-    });
-
-    return req.pipe(busboy);
+router.post('/save', upload.single('profile'), function(req, res) {
+    var formdata = {
+        'username': req.body.username,
+        'email': req.body.email,
+        'filesrc': req.file.path
+    };
+    console.log(formdata);
+    res.render('save', { formdata: formdata });
 });
 
 app.use('/app', router);
